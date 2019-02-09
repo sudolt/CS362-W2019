a6f02b01ae79b0c2bad286205bd99d590cf9a67f
Tim Sudol <sudolt@oregonstate.edu>
2019-02-09 07:06:32 -0800

    Quiz complete

---
+++ projects/sudolt/quiz/testme.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+   // DID: rewrote this function
+   // Limiting the random to a string with the required characters:
+   // '[', '(', '{', ' ', 'a', 'x', '}', ')', ']'
+
+   char charTest[10] = "[({ ax})]";
+   char c = charTest[rand() % 9];
+
+   return c;
+}
+
+char *inputString()
+{
+   // DID: rewrote this function
+   // Limiting the random to a string with the required characters:
+   // 'r', 'e', 's', 't'
+
+   char stringTest[5] = "rest";
+   char stringResult[6] = {0};
+
+   int x;
+
+   for (x = 0; x < 5; ++x)
+   {
+      stringResult[x] = stringTest[rand() % 4];
+   }
+
+   printf("String = %s\n", stringResult);
+
+   return stringResult;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
